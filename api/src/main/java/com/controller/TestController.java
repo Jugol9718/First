ded7884af491277328904488dcb04d3aabb0918d
1373262611@qq.com <1373262611@qq.com>
2019-10-23 09:14:21 +0800

    ...

--- api/src/main/java/com/controller/TestController.java
+++ api/src/main/java/com/controller/TestController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author zlp
